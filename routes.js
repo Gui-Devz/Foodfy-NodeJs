36a16f1d0655e9a82a87b890e945ce2016f47eec
Guilherme <guilherme_devz@hotmail.com>
2020-04-27 22:20:48 -0300

    code refatored and starting recipe page

---
+++ routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const data = require("./assets/data");
+
+const routes = express.Router();
+
+routes.get("/", (req, res) => {
+    return res.render("index", { recipes: data });
+});
+
+routes.get("/about", (req, res) => {
+    return res.render("about");
+});
+
+routes.get("/recipes", (req, res) => {
+    return res.render("recipes", { recipes: data });
+});
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const routes = require("./routes");
 
 const server = express();
 
 server.use(express.static("public"));
 server.use(express.static("img"));
+server.use(routes);
 
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
     express: server,
     noCache: true,
-});
-
-server.get("/", (req, res) => {
-    return res.render("index");
-});
-
-server.get("/about", (req, res) => {
-    return res.render("about");
-});
-
-server.get("/recipes", (req, res) => {
-    return res.render("recipes");
 });
 
 server.listen(5000, () => {
