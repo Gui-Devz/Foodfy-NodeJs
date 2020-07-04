15cc3b219f04e5621b771154b3eceaa331cd6be1
Guilherme <guilherme_devz@hotmail.com>
2020-07-03 21:22:01 -0300

    Adjusting the data file

--- routes.js
+++ routes.js
@@ -4,25 +4,25 @@ const data = require("./assets/data");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-    return res.render("index", { recipes: data });
+  return res.render("index", { recipes: data });
 });
 
 routes.get("/about", (req, res) => {
-    return res.render("about");
+  return res.render("about");
 });
 
 routes.get("/recipes", (req, res) => {
-    return res.render("recipes", { recipes: data });
+  return res.render("recipes", { recipes: data });
 });
 
 routes.get("/recipe", (req, res) => {
-    const id = req.query.id;
+  const id = req.query.id;
 
-    const recipe = data.find((recipe) => {
-        return recipe.id == id;
-    });
+  const recipe = data.find((recipe) => {
+    return recipe.id == id;
+  });
 
-    return res.render("recipe", { recipe });
+  return res.render("recipe", { recipe });
 });
 
 module.exports = routes;
