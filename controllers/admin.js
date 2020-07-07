cdf323d35925437b7303b2112b50b58e83f3bb1d
Guilherme <guilherme_devz@hotmail.com>
2020-07-07 20:19:05 -0300

    Created the PUT route logic

--- controllers/admin.js
+++ controllers/admin.js
@@ -31,12 +31,37 @@ exports.edit = (req, res) => {
 };
 
 exports.put = (req, res) => {
-  const { id, image, ingredients, steps, information } = req.body;
+  const { id, image, ingredients, preparation, information } = req.body;
   const keys = Object.keys(req.body);
 
+  console.log(req.body);
   for (const key of keys) {
     if (key == "") {
       return res.send("Fill all the fields!");
     }
   }
+
+  let foundIndex = 0;
+  const recipe = data.recipes.find((recipe, index) => {
+    if (recipe.id == id) {
+      foundIndex = index;
+      return true;
+    }
+  });
+
+  if (!recipe) return res.send("Recipe not found!");
+
+  data.recipes[foundIndex] = {
+    ...recipe,
+    ...req.body,
+    id: Number(id),
+  };
+
+  fs.writeFile("./assets/data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      return res.send("Error writting file!");
+    } else {
+      return res.redirect(`/admin/recipes/${id}`);
+    }
+  });
 };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require("./routes");
 
 const server = express();
 
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
 server.use(express.static("img"));
 server.use(express.static("scripts"));
